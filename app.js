8f1a5ee7d8665c278b52399d4266082b058326da
Mihkel Sokk <mihkelsokk@gmail.com>
2012-04-27 16:32:14 +0300

    Map pin test

--- app.js
+++ app.js
@@ -13,7 +13,29 @@ var server = app.listen('4000')
 
 
 io.sockets.on('connection', function(socket) {
-  console.log(socket);
+
+  io.sockets.clients().forEach(function(s) {
+    s.get('coordinates', function(err, coords) {
+      if(coords != null) {
+        console.log('client connected, sending list');
+        socket.emit('coordinates', s.id, coords);
+      }
+    });
+  });
+
+
+
+  socket.on('coordinates', function(coords) {
+    socket.set('coordinates', coords, function() {
+      socket.broadcast.emit('coordinates', socket.id, coords);
+    });
+
+  });
+
+
+  socket.on('disconnect', function () {
+    io.sockets.emit('user disconnected', socket.id);
+  });
 });
 
 console.log('Callapp running at port 4000');
---
+++ public/callapp.js
@@ -0,0 +1,91 @@
+var CallMap = function() {
+  var that = this;
+  this.markers = {};
+
+  var socket = window.socket = io.connect();
+
+
+  var map = new google.maps.Map(document.getElementById("map_canvas"), {
+    center: new google.maps.LatLng(59.434754,24.758284),
+    zoom: 12,
+    streetViewControl: false,
+    panControl: false,
+    mapTypeControl: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [
+      {
+        stylers: [
+          { gamma: 0.5 },
+          { lightness: 15 },
+          { hue: "#00b2ff" },
+          { visibility: "on" }
+        ]
+      }
+    ]
+  });
+
+  socket.on('coordinates', function(id, coords) {
+    console.log(arguments);
+    var latlng = new google.maps.LatLng(coords.latitude, coords.longitude);
+
+    that.markers[id] = new google.maps.Marker({
+      position: latlng,
+      animation: google.maps.Animation.DROP,
+      map: map,
+      icon: "/img/marker.png",
+      shadow: new google.maps.MarkerImage(
+        '/img/shadow.png',
+        new google.maps.Size(42,26),
+        new google.maps.Point(0,0),
+        new google.maps.Point(13,26)
+      ),
+      title: id
+    });
+  });
+
+  socket.on('user disconnected', function(id) {
+    that.markers[id].setMap(null);
+    delete that.markers[id];
+  });
+
+
+  this.on('position', function(pos) {
+    var latlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+    map.setCenter(latlng);
+    socket.emit('coordinates', pos.coords);
+
+    that.markers[socket.socket.sessionid] = new google.maps.Marker({
+        position: latlng,
+        animation: google.maps.Animation.DROP,
+        map: map,
+        icon: "/img/marker.png",
+        shadow: new google.maps.MarkerImage(
+          '/img/shadow.png',
+          new google.maps.Size(42,26),
+          new google.maps.Point(0,0),
+          new google.maps.Point(13,26)
+        ),
+        title: socket.socket.sessionid
+
+    });
+
+
+  });
+
+  this.getLocation();
+
+};
+io.util.mixin(CallMap, io.EventEmitter);
+
+CallMap.prototype.getLocation = function() {
+  var that = this;
+  navigator.geolocation.getCurrentPosition(function(position) {
+    that.emit('position', position);
+  }, function(error) {
+
+  });
+};
+
+
+
+window.cm = new CallMap();
